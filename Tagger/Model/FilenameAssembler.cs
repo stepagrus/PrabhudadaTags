641c7108b2b9421d38d6ce498e0e1083ab60ddfb
stepagrus <stepagrus@mail.ru>
2018-04-29 15:57:19 +0600

    refactoring

--- Tagger/Model/FilenameAssembler.cs
+++ Tagger/Model/FilenameAssembler.cs
@@ -15,48 +15,6 @@ namespace Tagger.Model
             Disassemble(filename);
         }
 
-        public void Disassemble(string filename)
-        {
-            var items = filename.Split(new string[] { Delimiter }, StringSplitOptions.None);
-
-            var dateComponents = items[0].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-
-            bool isIncorrentDate = dateComponents.Length < 1 || dateComponents.Length > 2;
-            bool isIncorrectFileNameFormat = items.Length < 3;
-            if (isIncorrentDate || isIncorrectFileNameFormat)
-            {
-                _isValid = false;
-                return;
-            }
-            else
-            {
-                _isValid = true;
-            }
-
-            Date = dateComponents[0];
-            if (dateComponents.Length == 2)
-                Number = dateComponents[1];
-
-            City = items[1];
-            Shastra = items[2];
-
-            if (items.Length > 3)
-                Title = String.Join(Delimiter, items.GetSubArrayStartAt(3));
-        }
-
-        private string Assemble()
-        {
-            List<string> items = new List<string>();
-            string _date = Date;
-            if (!String.IsNullOrEmpty(Number))
-                _date += " " + Number;
-            items.Add(_date);
-            items.Add(City);
-            items.Add(Shastra);
-            items.Add(Title);
-            return String.Join(Delimiter, items);
-        }
-
         public void SetNumber(int number)
         {
             Number = $"#{number}";
@@ -106,6 +64,49 @@ namespace Tagger.Model
         public string City { get; set; } = String.Empty;
         public string Shastra { get; set; } = String.Empty;
         public string Title { get; set; } = String.Empty;
+
+
+        private void Disassemble(string filename)
+        {
+            var items = filename.Split(new string[] { Delimiter }, StringSplitOptions.None);
+
+            var dateComponents = items[0].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            bool isIncorrentDate = dateComponents.Length < 1 || dateComponents.Length > 2;
+            bool isIncorrectFileNameFormat = items.Length < 3;
+            if (isIncorrentDate || isIncorrectFileNameFormat)
+            {
+                _isValid = false;
+                return;
+            }
+            else
+            {
+                _isValid = true;
+            }
+
+            Date = dateComponents[0];
+            if (dateComponents.Length == 2)
+                Number = dateComponents[1];
+
+            City = items[1];
+            Shastra = items[2];
+
+            if (items.Length > 3)
+                Title = String.Join(Delimiter, items.GetSubArrayStartAt(3));
+        }
+
+        private string Assemble()
+        {
+            List<string> items = new List<string>();
+            string _date = Date;
+            if (!String.IsNullOrEmpty(Number))
+                _date += " " + Number;
+            items.Add(_date);
+            items.Add(City);
+            items.Add(Shastra);
+            items.Add(Title);
+            return String.Join(Delimiter, items);
+        }
 
         private bool _isValid;
 
